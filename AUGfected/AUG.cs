c238534c4b54059d7a482f585d3ff3125f1da2d6
Slvr99 <bsawii@gmail.com>
2019-04-27 19:27:52 -0400

    Add project files.

---
+++ AUGfected/AUG.cs
@@ -0,0 +1,30 @@
+﻿using InfinityScript;
+
+namespace CustomWeapon
+{
+    public class CustomWeapon : BaseScript
+    {
+        public CustomWeapon()
+        {
+            PlayerConnected += onPlayerConnected;
+        }
+        private static void onPlayerConnected(Entity player)
+        {
+            if (player.SessionTeam == "allies")
+            {
+                player.GiveWeapon("iw5_m60jugg_mp_thermal_silencer_camo07");
+                player.SetPerk("specialty_lightweight", true, true);
+                return;
+            }
+            player.SpawnedPlayer += () => onPlayerSpawned(player);
+        }
+        private static void onPlayerSpawned(Entity player)
+        {
+            if (player.SessionTeam == "allies")
+            {
+                player.GiveWeapon("iw5_m60jugg_mp_thermal_silencer_camo07");
+                player.SetPerk("specialty_lightweight", true, true);
+            }
+        }
+    }
+}
